175254ead9c3b448d8b792d3da50b4e2fcc25faf
yayner2002 <yayner2002@gmail.com>
2023-07-26 19:02:14 +0300

    Update order to paid controller

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -66,14 +66,30 @@ const getOrderById = asyncHandler(async (req, res) => {
 });
 
 // @desc    Update order to paid
-// @route   GET /api/orders/:id/pay
+// @route   PUT /api/orders/:id/pay
 // @access  Private
-const updateOrderToPaid = asyncHandler((req, res) => {
-  res.send("update order to paid");
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    }; // this data comes from the paypal API
+
+    const updatedOrder = await order.save();
+    res.status(200).json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
 });
 
 // @desc    Update order to delivered
-// @route   GET /api/orders/:id/deliver
+// @route   PUT /api/orders/:id/deliver
 // @access  Private/Admin
 const updateOrderToDelivered = asyncHandler((req, res) => {
   res.send("update order to delivered");
