da5053ec54883d0b7366ce84a05884bb5427a641
yayner2002 <yayner2002@gmail.com>
2023-07-26 18:50:37 +0300

    Paypal setup

--- frontend/src/Screens/OrderScreen.js
+++ frontend/src/Screens/OrderScreen.js
@@ -32,10 +32,10 @@ const OrderScreen = () => {
             <ListGroup.Item>
               <h2>Shipping</h2>
               <p>
-                <strong>Name:</strong> {order.user.name},{" "}
+                <strong>Name:</strong> {order.user.name}
               </p>
               <p>
-                <strong>Email:</strong> {order.user.email},{" "}
+                <strong>Email:</strong> {order.user.email}
               </p>
               <p>
                 <strong>Adress:</strong> {order.shippingAddress.address},
