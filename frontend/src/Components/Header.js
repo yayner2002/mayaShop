a955a7b9b3853ec860bcde647292dcb13901e4a8
yayner2002 <yayner2002@gmail.com>
2023-07-31 19:00:23 +0300

    Add admin route component

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -62,7 +62,7 @@ const Header = () => {
                 </LinkContainer>
               )}
               {userInfo && userInfo.isAdmin && (
-                <NavDropdown title="Admin" id="adminmenu">
+                <NavDropdown title="Admin" id="adminmenu" >
                   <LinkContainer to="/admin/productlist">
                     <NavDropdown.Item>Products</NavDropdown.Item>
                   </LinkContainer>
