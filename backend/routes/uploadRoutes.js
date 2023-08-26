2d0bf7e1d7fcc6710b8afb1a83ea3f2ffb073ee2
yayner2002 <yayner2002@gmail.com>
2023-08-26 12:18:25 +0300

    Add multer and image upload endpoint

---
+++ backend/routes/uploadRoutes.js
@@ -0,0 +1,50 @@
+import path from "path";
+import express from "express";
+import multer from "multer";
+const router = express.Router();
+
+// which storage engine to use where our files will be stored
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    // cb = callback
+    cb(null, "uploads/"); // folder where we want to upload our files
+  },
+  filename(req, file, cb) {
+    cb(
+      null,
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+    ); // file.fieldname = name of the file, Date.now() = current date, path.extname(file.originalname) = extension of the file
+  },
+});
+
+// function to check if the file is an image or not
+function checkFileType(file, cb) {
+  const fileTypes = /jpg|jpeg|png/; // regular expression
+  const extname = fileTypes.test(path.extname(file.originalname).toLowerCase()); // test if the extension name is in the fileTypes array
+  const mimetype = fileTypes.test(file.mimetype); // test if the mimetype is in the fileTypes array
+
+  if (extname && mimetype) {
+    return cb(null, true); // if the file is an image
+  } else {
+    cb("Images only!"); // if the file is not an image
+  }
+}
+
+// upload middleware
+const upload = multer({
+  storage,
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+});
+
+// upload route
+router.post("/", upload.single("image"), (req, res) => {
+  res.send({
+    message: "Image uploaded successfully",
+    image: `/${req.file.path}`,
+  });
+});
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -8,6 +9,7 @@ import orderRoutes from "./routes/orderRoutes.js";
 import connectDb from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 connectDb();
+import uploadRoutes from "./routes/uploadRoutes.js";
 
 const app = express();
 // body parser middleware to get the body data
@@ -29,6 +31,11 @@ app.use("/api/orders", orderRoutes);
 app.use("/api/config/paypal", (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
+app.use("/api/upload", uploadRoutes);
+
+// make the uploads folder static so that we can access the files inside it
+const __dirname = path.resolve(); // set the __dirname variable to the current directory name
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 app.use(notFound);
 app.use(errorHandler);
