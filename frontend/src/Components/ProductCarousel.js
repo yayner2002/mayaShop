37598b6a4e71bf151ca1cf21d82d433999a1d873
yayner2002 <yayner2002@gmail.com>
2023-08-28 17:53:03 +0300

    Fixed the double spinner

--- frontend/src/Components/ProductCarousel.js
+++ frontend/src/Components/ProductCarousel.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { useGetTopRatedProductsQuery } from "../slices/productsApiSlice";
-import LoadingSpinner from "./LoadingSpinner";
 import MessageAlert from "./MessageAlert";
 import { Carousel, Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
@@ -9,25 +8,25 @@ const ProductCarousel = () => {
   const { data: products, isLoading, error } = useGetTopRatedProductsQuery();
   return (
     <>
-      {isLoading ? (
-        <LoadingSpinner />
-      ) : error ? (
-        <MessageAlert variant="danger">{error?.data?.message}</MessageAlert>
+      { isLoading ? ("") : error ? (
+        <MessageAlert variant="danger">{error}</MessageAlert>
       ) : (
-        <Carousel pause="hover" className="bg-primary mb-4">
-          {products.map((product) => (
-            <Carousel.Item key={product._id}>
-              <Link to={`/products/${product._id}`}>
-                <Image src={product.image} alt={product.name} fluid />
-                <Carousel.Caption className="carousel-caption">
-                  <h2>
-                    {product.name}({product.price})
-                  </h2>
-                </Carousel.Caption>
-              </Link>
-            </Carousel.Item>
-          ))}
-        </Carousel>
+        <>
+          <Carousel pause="hover" className="bg-primary mb-4">
+            {products.map((product) => (
+              <Carousel.Item key={product._id}>
+                <Link to={`/products/${product._id}`}>
+                  <Image src={product.image} alt={product.name} fluid />
+                  <Carousel.Caption className="carousel-caption">
+                    <h2>
+                      {product.name}({product.price})
+                    </h2>
+                  </Carousel.Caption>
+                </Link>
+              </Carousel.Item>
+            ))}
+          </Carousel>
+        </>
       )}
     </>
   );
